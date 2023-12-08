833762068502f509dee3825bb22ed1fc45fc1b46
july1012 <113192987+july1012@users.noreply.github.com>
2023-12-07 21:22:47 -0500

    setup

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,24 +1,9 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+async function data() {
+  let get = await fetch("http://makeup-api.herokuapp.com/api/v1/products.json");
+  let results = await get.json();
+  console.log(results);
+}
 
-setupCounter(document.querySelector('#counter'))
+data();
