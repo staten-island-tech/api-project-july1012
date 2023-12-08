eb689f8636e64a64759e76cd6f9b0d7265b694d6
july1012 <113192987+july1012@users.noreply.github.com>
2023-12-08 10:17:08 -0500

    fvgfdr

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,9 +1,10 @@
 async function data() {
   let get = await fetch(
-    "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline&product_type=lipstick&price_less_than=10"
+    "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline&product_type=eyeliner&price_less_than=10"
   );
   let results = await get.json();
   console.log(results);
+  return results.json;
 }
 
 data();
