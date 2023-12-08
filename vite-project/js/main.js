ac65c8df4e2dca3aa44bd393471b4785666ff9ab
july1012 <113192987+july1012@users.noreply.github.com>
2023-12-07 21:34:28 -0500

    slkfnv lkfs

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,7 +1,7 @@
-import "./style.css";
-
 async function data() {
-  let get = await fetch("http://makeup-api.herokuapp.com/api/v1/products.json");
+  let get = await fetch(
+    "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline&product_type=lipstick&price_less_than=10"
+  );
   let results = await get.json();
   console.log(results);
 }
