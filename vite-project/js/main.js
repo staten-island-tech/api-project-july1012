b5351334b6a35ee73005b71723a40259e15b2f89
july1012 <113192987+july1012@users.noreply.github.com>
2024-01-01 23:40:35 -0500

    sdfnsdocfnhdsoujhcno

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,31 +1,80 @@
 import "../css/style.css";
+let dataa = [];
 
-const url = 'http://makeup-api.herokuapp.com/api/v1/products.json';
+const url = "http://makeup-api.herokuapp.com/api/v1/products.json";
 async function data(url) {
-  let get = await fetch(url);
-  let results = await get.json();
-  console.log(results);
-  return results.json;
+  try {
+    const response = await fetch(url);
+    dataa = await response.json();
+    console.log(dataa);
+  } catch (error) {
+    console.log(error);
+  }
+
+  // let results = await response.json();
+  // console.log(results);
+  // return results.json;
 }
 data(url);
 
 const DOMSelectors = {
   header: document.querySelector(".header"),
   form: document.querySelector("#form"),
-  brand:document.querySelector("#brand"),
-  product: document.querySelector("#product"),
-  ten: document.querySelector(".ten"),
-  twenty: document.querySelector(".twenty"),
-  fifty: document.querySelector(".fifty"),
+  types: document.querySelectorAll('input[name="type"]'),
+  checkAll: document.querySelector("#checkAll"),
+  // brand: document.querySelector("#brand"),
+  // product: document.querySelector("#product"),
+  // ten: document.querySelector(".ten"),
+  // twenty: document.querySelector(".twenty"),
+  // fifty: document.querySelector(".fifty"),
 };
 
-let prices = [DOMSelectors.ten, DOMSelectors.twenty, DOMSelectors.fifty]
+let checkAll = false;
 
-prices.forEach((btn)=> btn.addEventListener("click",function(){
-  const money = btn.textContent;
-  console.log(money);
-  
-}))
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let bundle = [];
+  let type = document.querySelectorAll('input[name="type"]:checked');
+  let tipuhs = [];
+  let random = "";
 
+  type.forEach((twaipe) => tipuhs.push(twaipe.id));
 
+  //randomize selections
+  tipuhs.forEach(function (type) {
+    console.log(type);
+    let newtype = dataa.filter((makeup) => makeup.product_type === type);
+    console.log(newtype);
+    random = Math.floor(Math.random() * newtype.length);
+    bundle.push(newtype[random]);
+  });
+  console.log("bundle", bundle);
+});
 
+DOMSelectors.checkAll.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (checkAll === false) {
+    DOMSelectors.types.forEach((checkbox) => {
+      checkbox.checked = true;
+    });
+    checkAll = true;
+    DOMSelectors.checkAll.innerHTML = "Uncheck All";
+  } else if (checkAll === true) {
+    DOMSelectors.types.forEach((checkbox) => {
+      checkbox.checked = false;
+    });
+    checkAll = false;
+    DOMSelectors.checkAll.innerHTML = "Check All";
+  } else {
+    console.log("Error checking all");
+  }
+});
+//
+
+// let prices = [DOMSelectors.ten, DOMSelectors.twenty, DOMSelectors.fifty];
+// prices.forEach((btn) =>
+//   btn.addEventListener("click", function () {
+//     const money = btn.textContent;
+//     console.log(money);
+//   })
+// );
