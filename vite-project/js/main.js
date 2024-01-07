1d00fec2ffa593ca5cd7d0eee156bc6f5d7c9f28
july1012 <113192987+july1012@users.noreply.github.com>
2024-01-07 16:15:35 -0500

    sfxvgdzs

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -7,8 +7,8 @@ async function data(url) {
     const response = await fetch(url);
     const dataa = await response.json();
     insert(dataa);
-    console.log('Data:', dataa[0])
-    getData(dataa)
+    console.log("Data:", dataa[0]);
+    getData(dataa);
   } catch (error) {
     console.log(error);
   }
@@ -21,6 +21,7 @@ const DOMSelectors = {
   types: document.querySelectorAll('input[name="type"]'),
   checkAll: document.querySelector("#checkAll"),
   cardsholder: document.querySelector("#cards-holder"),
+  formanswer: document.querySelector("#formanswer"),
 };
 
 function insert(cards) {
@@ -41,38 +42,39 @@ function insert(cards) {
 }
 
 function getData(dataa) {
-DOMSelectors.form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  let bundle = [];
-  let type = document.querySelectorAll('input[name="type"]:checked');
-  let tipuhs = [];
-  let random = "";
+  DOMSelectors.form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let bundle = [];
+    let type = document.querySelectorAll('input[name="type"]:checked');
+    let tipuhs = [];
+    let random = "";
 
-  type.forEach((twaipe) => tipuhs.push(twaipe.id));
+    type.forEach((twaipe) => tipuhs.push(twaipe.id));
 
-  //randomize selections of products
-  tipuhs.forEach(function (type) {
-    console.log(type);
-    dataa.forEach((makeup) => console.log(makeup))
-    let newtype = dataa.filter((makeup) => makeup.product_type === type);
-    // console.log('Filtered Data:', newtype);
-    random = Math.floor(Math.random() * newtype.length);
-    bundle.push(newtype[random]);
-  });
-  console.log("bundle", bundle);
-  bundle.forEach((basket) => {
-  DOMSelectors.cardsholder.insertAdjacentHTML(
-      "beforeend",
-      `<div class="cards">
+    //randomize selections of products
+    tipuhs.forEach(function (type) {
+      console.log(type);
+      dataa.forEach((makeup) => console.log(makeup));
+      let newtype = dataa.filter((makeup) => makeup.product_type === type);
+      // console.log('Filtered Data:', newtype);
+      random = Math.floor(Math.random() * newtype.length);
+      bundle.push(newtype[random]);
+    });
+    console.log("bundle", bundle);
+    bundle.forEach((basket) => {
+      DOMSelectors.formanswer.insertAdjacentHTML(
+        "beforeend",
+        `<div class="cards">
        <h2 class="companyname">${basket.brand}:</h2>
       <h3 class="title">${basket.name}</h3>
       <img src= "${basket.api_featured_image}" class="pic">
-     <h4 class="desc">${basket.tag_list}</h4>
+     <h4 class="desc">$${basket.price}</h4>
       </div>
        `
-    );
+      );
+    });
   });
-});}
+}
 
 //check all and uncheck all button
 let checkAll = false;
